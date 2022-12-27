730d3187cc2f1617f49201b49bcefe483a0fecb8
yebin <yebin5127@gmail.com>
2022-12-28 08:36:53 +0900

    Update user entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,27 +1,31 @@
-import { 
-    Entity, 
-    PrimaryGeneratedColumn, 
-    Column,
-    CreateDateColumn,
-    OneToMany,
-    OneToOne
-} from "typeorm"
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+  OneToOne,
+  UpdateDateColumn,
+} from "typeorm";
 import { Bouquet } from "./bouquet.entity";
 
-@Entity({ name:  'user' })
+@Entity({ name: "user" })
 export class User {
-    @PrimaryGeneratedColumn()
-    id!: number;
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-    @Column({ unique: true, nullable: false })
-    email!: string;
+  @Column({ unique: true, nullable: false })
+  email!: string;
 
-    @Column({ nullable: false })
-    nickname!: string;
+  @Column({ nullable: false })
+  nickname!: string;
 
-    @Column({ nullable: false })
-    password!: string; 
+  @Column({ nullable: false })
+  password!: string;
 
-    @OneToOne(() => Bouquet, (bouquet) => bouquet.user)
-    bouquet!: Bouquet[];
-};
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @OneToOne(() => Bouquet, (bouquet) => bouquet.user)
+  bouquet!: Bouquet[];
+}
