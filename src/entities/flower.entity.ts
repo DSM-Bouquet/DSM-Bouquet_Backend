e4a4abef1b33df53d1008850b013f1687d341cac
yebin <yebin5127@gmail.com>
2022-12-28 08:36:59 +0900

    Update flower entity

--- src/entities/flower.entity.ts
+++ src/entities/flower.entity.ts
@@ -1,27 +1,39 @@
-import { 
-    Column,
-    Entity, 
-    JoinColumn, 
-    ManyToOne,
-    PrimaryGeneratedColumn 
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
 } from "typeorm";
 import { Bouquet } from "./bouquet.entity";
 
-@Entity({ name: 'flower' })
+@Entity({ name: "flower" })
 export class Flower {
-    @PrimaryGeneratedColumn()
-    id!: number;
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-    @Column({ nullable: false })
-    bouquet_id!: number;
+  @Column({ nullable: false })
+  bouquet_id!: number;
 
-    @Column({ nullable: false, charset: 'utf8mb4' })
-    nickname!: string;
+  @Column({
+    nullable: false,
+    charset: "utf8mb4",
+    collation: "utf8mb4_general_ci",
+  })
+  nickname!: string;
 
-    @Column({ nullable: false, charset: 'utf8mb4' })
-    content!: string;
+  @Column({
+    nullable: false,
+    charset: "utf8mb4",
+    collation: "utf8mb4_general_ci",
+  })
+  content!: string;
 
-    @ManyToOne(() => Bouquet, (bouquet) => bouquet.flower)
-    @JoinColumn({ name: 'bouquet_id' })
-    bouquet!: Bouquet;
-};
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @ManyToOne(() => Bouquet, (bouquet) => bouquet.flower)
+  @JoinColumn({ name: "bouquet_id" })
+  bouquet!: Bouquet;
+}
